38f05710e336451a4e33dd9c87c62e1f9def78ee
Gabriel Bagon <gabriel.bagon@smartdata.science>
2022-06-22 11:43:47 -0300

    feature: creating the question class

--- src/model/question.ts
+++ src/model/question.ts
@@ -1,3 +1,31 @@
 export default class QuestionModel {
-    
+    #id: number
+    #statement: string
+    #answers: any[]
+    #correct: boolean
+    // #answered: boolean
+
+    constructor(id: number, statement: string, answers: any[], correct: boolean = false) {
+        this.#id = id
+        this.#statement = statement
+        this.#answers = answers
+        this.#correct = correct
+    }
+
+    get id() { 
+        return this.#id
+     }
+     get statement() {
+        return this.#statement
+     }
+     get answers() { 
+        return this.#answers
+     }
+     get correct() { 
+        return this.#correct
+     }
+     get answered() { 
+        //TODO: Implement this method
+        return false
+     }
 }
